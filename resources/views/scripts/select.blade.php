7d0fd3808eb6f77f6ccab4c371fea0ed98f7f1c5
Edwin Xu <edwinhuish@gmail.com>
2021-10-12 21:03:04 +0800

    为 select 增加 depends 方法（当ajax模式才有效）

--- resources/views/scripts/select.blade.php
+++ resources/views/scripts/select.blade.php
@@ -1,6 +1,7 @@
 <script>
 @section('admin.select-ajax')
     @if(isset($ajax))
+        var _this = $('{{ $selector }}');
         configs = $.extend(configs, {
         ajax: {
             url: "{{ $ajax['url'] }}",
@@ -32,6 +33,10 @@
             return markup;
         }
     });
+    @if(isset($depends))
+    _this.data('selectConfigs', $.extend({}, configs));
+    delete configs.ajax;
+    @endif
     @endif
 @overwrite
 </script>
@@ -56,6 +61,66 @@
         });
     });
     $(selector).trigger('change');
+</script>
+@endif
+
+@if(isset($depends) && isset($ajax))
+{{--depends联动--}}
+<script once>
+    var _this = $('{!! $selector !!}');
+    var fields = {!! $depends['fields'] !!};
+    var form = _this.closest('form');
+
+    var getFormFieldsVal = function(form, fields){
+        var formData = $(form).serializeArray();
+        var params = {};
+
+        for (var field of fields) {
+            for (var data of formData) {
+                if (!data.value.length) {
+                    continue;
+                }
+                if (data.name === field) {
+                    params[field] = data.value
+                }else if (data.name === field + '[]') {
+                    if(!Array.isArray(params[field])){
+                        params[field] = []
+                    }
+                    params[field].push(data.value);
+                }
+            }
+            if (!params.hasOwnProperty(field)){
+                return false;
+            }
+        }
+        return params;
+    }
+
+    var getSourceUrl = function(url, params){
+        return url + (url.match(/\?/)?'&':'?') + (new URLSearchParams(params)).toString();
+    }
+
+    @if($depends['clear'])
+    $.map(fields, function (field) {
+        var _selectors = [
+            '[name="' + field + '"]',
+            '[name="' + field + '[]"]'
+        ];
+        $.map(_selectors, function(_selector){
+            form.off('change.depends', _selector)
+                .on('change.depends', _selector, function () {
+                    _this.val(null).trigger('change');
+
+                    var params = getFormFieldsVal(form, fields);
+                    var configs = $.extend({}, _this.data('selectConfigs'));
+
+                    configs.ajax = !params ? {} : $.extend(configs.ajax, { url: getSourceUrl("{{ $ajax['url'] }}", params) });
+
+                    _this.select2(configs)
+                });
+        })
+    });
+    @endif
 </script>
 @endif
 
