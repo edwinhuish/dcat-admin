f4ab53b448c9879866fedf930adef181f11fecfe
Edwin Xu <edwinhuish@gmail.com>
2022-01-27 12:17:37 +0800

    增加config的获取

--- src/Application.php
+++ src/Application.php
@@ -3,6 +3,7 @@
 namespace Dcat\Admin;
 
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Traits\Macroable;
 
@@ -125,6 +126,67 @@ class Application
     public function getRoute(?string $route, array $params = [], $absolute = true)
     {
         return route($this->getRoutePrefix().$route, $params, $absolute);
+    }
+
+    /**
+     * @param  string|array  $key
+     * @param  null  $default
+     * @return $this|array|\ArrayAccess|mixed
+     */
+    public function config($key, $default = null)
+    {
+        return $this->appConfig($this->name, $key, $default);
+    }
+
+    /**
+     * @param  string  $app
+     * @param  string|array  $key
+     * @param  mixed|null  $default
+     * @return $this|array|\ArrayAccess|mixed
+     */
+    public function appConfig(string $app, $key, $default = null)
+    {
+        if (is_array($key)) {
+            return $this->setAppConfigs($app, $key);
+        }
+
+        return Arr::get($this->getAppConfigs($app), $key, $default);
+    }
+
+    /**
+     * @param  string|null  $app
+     * @return array
+     */
+    protected function getAppConfigs(string $app = null): array
+    {
+        if (! in_array($app, array_keys($this->getApps()))) {
+            return [];
+        }
+
+        if (! $app) {
+            $app = $this->name;
+        }
+
+        if (! isset($this->configs[$app])) {
+            $this->configs[$app] = config($app);
+        }
+
+        return $this->configs[$app];
+    }
+
+    /**
+     * @param  string  $app
+     * @param  array  $value
+     * @return $this
+     */
+    protected function setAppConfigs(string $app, array $value)
+    {
+
+        config([$app => $value]);
+
+        $this->configs[$app] = $value;
+
+        return $this;
     }
 
     protected function registerRoute(?string $app)
